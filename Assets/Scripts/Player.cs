c48dca16d47206f56580686bafc8a8ee3f8d5ab1
BoykoTV <tanya.titanya@gmail.com>
2020-04-14 13:02:11 +0300

    Flipping the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D myRigidbody;
+
     [SerializeField]
     private float movementSpeed;
+
+    private bool facingRight;
 
     // Start is called before the first frame update
     void Start()
     {
+        facingRight = true;
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -18,14 +22,23 @@ public class Player : MonoBehaviour
     void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
-
         HandleMovement(horizontal);
+        Flip(horizontal);
     }
 
     private void HandleMovement(float horizontal)
     {
         myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y);
+    }
 
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
 }
